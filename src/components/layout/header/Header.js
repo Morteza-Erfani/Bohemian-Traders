1dbe11dbd3e6104f0714a5b40ab89d69e6968a62
Morteza erfani <m.erfani74@gmail.com>
2022-08-28 19:59:18 +0430

    header style and animation complete except humberger menu

--- src/components/layout/header/Header.js
+++ src/components/layout/header/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 // Logo
@@ -13,41 +13,57 @@ import cartIcon from "../../../assets/carticon.png";
 import HumMenu from "./HumMenu";
 
 // Styles
-import styles from './Header.module.css'
+import styles from "./Header.module.css";
 
 const Header = () => {
+  const [humShow, setHumShow] = useState(false);
+
+  console.log(humShow);
+
   return (
-    <div className={styles.container}>
+    <header className={styles.container}>
       {/* top header over menu */}
-      <div>
+      <div className={styles.topHeader}>
         <p>FREE SHIPPING FOR ALL AUSTRALIAN ORDERS OVER $200</p>
       </div>
       {/* menu */}
-      <div>
+      <div className={styles.header}>
         {/* humberger menu */}
-        <div>
+        <div className={styles.hum}>
           {/* humberger icon */}
-          <div>
-            <div></div>
-            <div></div>
-            <div></div>
+
+          <div onClick={() => setHumShow(!humShow)} className={styles.humIcon}>
+            <div
+              className={humShow ? styles.firstCross : styles.firstHum}
+            ></div>
+            <div
+              className={humShow ? styles.secondCross : styles.secondHum}
+            ></div>
+            <div
+              className={humShow ? styles.thirdCross : styles.thirdHum}
+            ></div>
           </div>
-          <HumMenu />
+
+          {humShow && (
+            <div className={styles.humMenu}>
+              <HumMenu />
+            </div>
+          )}
         </div>
         {/* brand name in header */}
-        <div>
+        <div className={styles.brandName}>
           <Link to="/">
             <img src={logo} alt="logo" />
           </Link>
         </div>
         {/* header buttons */}
-        <div>
-          <img src={searchIcon} alt="search" />
-          <img src={profileIcon} alt="search" />
-          <img src={cartIcon} alt="search" />
+        <div className={styles.mobileRight}>
+          <img className={styles.button} src={searchIcon} alt="search" />
+          <img className={styles.button} src={profileIcon} alt="search" />
+          <img className={styles.button} src={cartIcon} alt="search" />
         </div>
       </div>
-    </div>
+    </header>
   );
 };
 
