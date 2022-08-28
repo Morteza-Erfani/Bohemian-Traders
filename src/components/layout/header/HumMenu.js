308a3950039d17efbb077d65e445b49e0329aa62
Morteza erfani <m.erfani74@gmail.com>
2022-08-28 22:50:35 +0430

    header and humberger menu style completed except menu links

--- src/components/layout/header/HumMenu.js
+++ src/components/layout/header/HumMenu.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "@apollo/client";
 
 // APIs
 import { GET_CATEGORIES_NAME } from "../../../graphql/queries";
 
 // Styles
-import styles from './HumMenu.module.css'
+import styles from "./HumMenu.module.css";
 
 const HumMenu = () => {
   const { loading, data, errors } = useQuery(GET_CATEGORIES_NAME);
+
+  const [show, setShow] = useState({
+    id: "",
+    isShow: false,
+  });
 
   console.log(data);
 
@@ -16,24 +21,50 @@ const HumMenu = () => {
     <div className={styles.container}>
       {data &&
         data.categories.map((category) => (
-          <div key={category.id}>
-            <div>
+          <div key={category.id} className={styles.categoryContainer}>
+            <div
+              className={styles.category}
+              onClick={() => {
+                console.log(show);
+                category.id === show.id
+                  ? setShow({ id: "", isShow: false })
+                  : setShow({ id: category.id, isShow: true });
+              }}
+            >
               <p>{category.name}</p>
               {category.collections.length ? (
                 <div>
-                  <p>+</p>
-                  <p>-</p>
+                  <p
+                    className={
+                      category.id === show.id ? styles.hide : styles.show
+                    }
+                  >
+                    +
+                  </p>
+                  <p
+                    className={
+                      category.id === show.id ? styles.show : styles.hide
+                    }
+                  >
+                    -
+                  </p>
                 </div>
               ) : (
                 <></>
               )}
             </div>
             {category.collections.length ? (
-              <div>
-                <p>VIEW ALL</p>
+              <div
+                className={
+                  category.id === show.id ? styles.collections : styles.hide
+                }
+              >
+                {category.name !== "CAMPAIGN" &&
+                  category.name !== "WHAT'S NEW" && <p>VIEW ALL</p>}
                 {category.collections.map((collection) => (
                   <p key={collection.id}>{collection.name}</p>
                 ))}
+                {category.name === "WHAT'S NEW" && <p>VIEW ALL</p>}
               </div>
             ) : (
               <></>
