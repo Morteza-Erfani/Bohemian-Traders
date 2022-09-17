72fb9e980634ae307cd44a51f47248a30d279bf5
Morteza erfani <m.erfani74@gmail.com>
2022-09-18 03:33:08 +0430

    reset password page created and styled

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Layout from "./components/layout/Index";
 // Components
 import HomePage from "./components/homePage/HomePage";
 import SignIn from "./components/signIn/SignIn";
+import ResetPassword from "./components/resetPassword/ResetPassword";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signIn" element={<SignIn />} />
+        <Route path="/reset" element={<ResetPassword />} />
         <Route path="/*" element={<Navigate to="/" />} />
       </Routes>
     </Layout>
---
+++ src/components/resetPassword/ResetPassword.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+// Styles
+import styles from "./ResetPassword.module.css";
+
+const ResetPassword = () => {
+  return (
+    <div className={styles.container}>
+      <h2>RESET PASSWORD</h2>
+      <p>
+        Fill in your email below to request a new password. An email will be
+        sent to the address below containing a link to verify your email
+        address.
+      </p>
+      <form className={styles.form}>
+        <label for="email">EMAIL ADDRESS</label>
+        <input id="email" type="email" />
+        <button type="submit">RESET PASSWORD</button>
+      </form>
+    </div>
+  );
+};
+
+export default ResetPassword;
