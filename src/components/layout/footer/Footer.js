2a0ebec9bd3be19fb3cbcbe6ae0a077910917574
Morteza erfani <m.erfani74@gmail.com>
2022-10-06 23:41:30 +0330

    footer is fully responsive

--- src/components/layout/footer/Footer.js
+++ src/components/layout/footer/Footer.js
@@ -27,105 +27,130 @@ const Footer = () => {
   return (
     <div id="footer">
       <div className={styles.container}>
-        <img src={logo} alt="logo" className={styles.logo} />
-        <ul className={styles.socialUl}>
-          <li>
-            <a>
-              <img
-                src={instagram}
-                className={styles.instagram}
-                alt="instagram"
+        <div className={styles.innerContainer}>
+          <div className={styles.footerCol}>
+            <img src={logo} alt="logo" className={styles.logo} />
+            <ul className={styles.socialUl}>
+              <li>
+                <a>
+                  <img
+                    src={instagram}
+                    className={styles.instagram}
+                    alt="instagram"
+                  />
+                </a>
+              </li>
+              <li>
+                <a>
+                  <img
+                    src={facebook}
+                    className={styles.facebook}
+                    alt="facebook"
+                  />
+                </a>
+              </li>
+              <li>
+                <a>
+                  <img
+                    src={pinterest}
+                    className={styles.pinterest}
+                    alt="pinterest"
+                  />
+                </a>
+              </li>
+              <li>
+                <a>
+                  <img src={tiktok} className={styles.tiktok} alt="tiktok" />
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.footerCol}>
+            <div
+              className={styles.downContainer}
+              onClick={() => showInfoHandler()}
+            >
+              <p>INFO</p>
+              <img src={down} alt="down" className={styles.down} />
+            </div>
+            <div
+              className={`${styles.links} ${
+                showInfo ? styles.show : styles.hide
+              }`}
+            >
+              <Link onClick={() => setShowInfo(false)} to="/about-us">
+                ABOUT US
+              </Link>
+              <Link onClick={() => setShowInfo(false)} to="/contact-us">
+                CONTACT US
+              </Link>
+              <Link onClick={() => setShowInfo(false)} to="/work-with-us">
+                WORK WITH US
+              </Link>
+              <Link onClick={() => setShowInfo(false)} to="/privacy-policy">
+                TS & CS
+              </Link>
+              <Link onClick={() => setShowInfo(false)} to="/privacy-policy">
+                PRIVACY POLICY
+              </Link>
+            </div>
+          </div>
+          <div className={styles.footerCol}>
+            <div
+              className={styles.downContainer}
+              onClick={() => ShowCustomCareHandler()}
+            >
+              <p>CUSTOMER CARE</p>
+              <img src={down} alt="down" className={styles.down} />
+            </div>
+            <div
+              className={`${styles.links} ${
+                showCustomCare ? styles.show : styles.hide
+              }`}
+            >
+              <Link
+                onClick={() => setShowCustomCare(false)}
+                to="/shipping-returns"
+              >
+                SHIPPING
+              </Link>
+              <Link
+                onClick={() => setShowCustomCare(false)}
+                to="/shipping-returns"
+              >
+                RETURNS
+              </Link>
+              <Link
+                onClick={() => setShowCustomCare(false)}
+                to="/inclusive-size-range"
+              >
+                INCLUSIVE SIZING
+              </Link>
+              <Link
+                onClick={() => setShowCustomCare(false)}
+                to="/flexible-payment-options"
+              >
+                PAYMENT METHODS
+              </Link>
+              <a>GIFT CARDS</a>
+              <a>OUTLET</a>
+            </div>
+          </div>
+          <div className={styles.footerCol}>
+            <p className={styles.subPara}>
+              SUBSCRIBE TO REVIEVE UPDATES AND SPECIAL OFFERS!
+            </p>
+            <form className={styles.inputContainer}>
+              <input
+                type="email"
+                inputMode="email"
+                placeholder="YOUR EMAIL ADDRESS"
+                className={styles.input}
               />
-            </a>
-          </li>
-          <li>
-            <a>
-              <img src={facebook} className={styles.facebook} alt="facebook" />
-            </a>
-          </li>
-          <li>
-            <a>
-              <img
-                src={pinterest}
-                className={styles.pinterest}
-                alt="pinterest"
-              />
-            </a>
-          </li>
-          <li>
-            <a>
-              <img src={tiktok} className={styles.tiktok} alt="tiktok" />
-            </a>
-          </li>
-        </ul>
-        <div className={styles.downContainer} onClick={() => showInfoHandler()}>
-          <p>INFO</p>
-          <img src={down} alt="down" className={styles.down} />
+              <button className={styles.subscribeBtn}>SUBSCRIBE</button>
+            </form>
+          </div>
         </div>
-        <div
-          className={`${styles.links} ${showInfo ? styles.show : styles.hide}`}
-        >
-          <Link onClick={() => setShowInfo(false)} to="/about-us">
-            ABOUT US
-          </Link>
-          <Link onClick={() => setShowInfo(false)} to="/contact-us">
-            CONTACT US
-          </Link>
-          <Link onClick={() => setShowInfo(false)} to="/work-with-us">
-            WORK WITH US
-          </Link>
-          <Link onClick={() => setShowInfo(false)} to="/privacy-policy">
-            TS & CS
-          </Link>
-          <Link onClick={() => setShowInfo(false)} to="/privacy-policy">
-            PRIVACY POLICY
-          </Link>
-        </div>
-        <div
-          className={styles.downContainer}
-          onClick={() => ShowCustomCareHandler()}
-        >
-          <p>CUSTOMER CARE</p>
-          <img src={down} alt="down" className={styles.down} />
-        </div>
-        <div
-          className={`${styles.links} ${
-            showCustomCare ? styles.show : styles.hide
-          }`}
-        >
-          <Link onClick={() => setShowCustomCare(false)} to="/shipping-returns">
-            SHIPPING
-          </Link>
-          <Link onClick={() => setShowCustomCare(false)} to="/shipping-returns">
-            RETURNS
-          </Link>
-          <Link
-            onClick={() => setShowCustomCare(false)}
-            to="/inclusive-size-range"
-          >
-            INCLUSIVE SIZING
-          </Link>
-          <Link
-            onClick={() => setShowCustomCare(false)}
-            to="/flexible-payment-options"
-          >
-            PAYMENT METHODS
-          </Link>
-          <a>GIFT CARDS</a>
-          <a>OUTLET</a>
-        </div>
-        <p className={styles.subPara}>
-          SUBSCRIBE TO REVIEVE UPDATES AND SPECIAL OFFERS!
-        </p>
-        <form className={styles.inputContainer}>
-          <input
-            type="email"
-            inputMode="email"
-            placeholder="YOUR EMAIL ADDRESS"
-            className={styles.input}
-          />
-          <button className={styles.subscribeBtn}>SUBSCRIBE</button>
-        </form>
         <p className={styles.contact}>
           CUSTOMERSERVICE@BOHEMIANTRADERS.COM | +61 2 4327 8640 | MON - FRI |
           9AM - 5PM AEST
