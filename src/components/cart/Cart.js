73f56d5b077fc244e8012732d05b31eb64341a69
Morteza erfani <m.erfani74@gmail.com>
2022-10-08 20:05:57 +0330

    cart page responsived and responsive bug in header fixed

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -19,6 +19,7 @@ import zip from "../../assets/zip-button-wht.svg";
 import paypal from "../../assets/paypal.svg";
 import gPay from "../../assets/dark_gpay.svg";
 import ChangeSizeModal from "./changeSizeModal/ChangeSizeModal";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
   const cart = useSelector((state) => state.cart.cart);
@@ -29,7 +30,7 @@ const Cart = () => {
 
   const [showChange, setShowChange] = useState(false);
 
-  const [selectedSizeForChange, setSelectedSizeForChange] = useState()
+  const [selectedSizeForChange, setSelectedSizeForChange] = useState();
 
   // console.log(totalPrice);
 
@@ -40,8 +41,8 @@ const Cart = () => {
   const ref = useRef(null);
 
   useEffect(() => {
-    document.title = 'Bohemian Traders - Shopping Cart'
-  },[])
+    document.title = "Bohemian Traders - Shopping Cart";
+  }, []);
 
   // const fillSpace = () => {
   //   const usedSpace =
@@ -59,6 +60,9 @@ const Cart = () => {
     return (
       <>
         <div className={styles.container} ref={ref}>
+          <p className={styles.location}>
+            <Link to="/">HOME</Link> / YOUR CART
+          </p>
           <h1 className={styles.header}>YOUR CART ({cart.length} ITEMS)</h1>
           <h3 className={styles.empty}>Your cart is empty</h3>
         </div>
@@ -69,6 +73,9 @@ const Cart = () => {
 
   return (
     <div className={styles.container}>
+      <p className={styles.location}>
+        <Link to="/">HOME</Link> / YOUR CART
+      </p>
       <h1 className={styles.header}>YOUR CART ({count} ITEMS)</h1>
       {cart.map((product) => (
         <div
@@ -76,114 +83,127 @@ const Cart = () => {
           key={`${product.id}${product.size}`}
         >
           <div className={styles.firstSection}>
-            <div className={styles.photoContainer}>
-              <img src={product.photo} alt="product" />
+            <p className={styles.title}>Item</p>
+            <div className={styles.innerFirstSection}>
+              <div className={styles.photoContainer}>
+                <img src={product.photo} alt="product" />
+              </div>
+              <div className={styles.titleContainer}>
+                <p className={styles.brandName}>Boheamian Traders</p>
+                <a>{product.name}</a>
+                <p className={styles.size}>
+                  Size: <span>{product.size}</span>
+                </p>
+                <button
+                  className={styles.change}
+                  onClick={() => {
+                    setShowChange(true);
+                    dispatch(selectSize({ size: product.size }));
+                    setSelectedSizeForChange(product.size);
+                  }}
+                >
+                  CHANGE
+                </button>
+                <ChangeSizeModal
+                  show={showChange}
+                  onClose={() => setShowChange(false)}
+                  allSize={product.allSize}
+                  name={product.name}
+                  onChangeSize={(size) => {
+                    dispatch(selectSize({ size: size }));
+                  }}
+                  onSave={() => {
+                    dispatch(
+                      changeSize({
+                        id: product.id,
+                        prevSize: selectedSizeForChange,
+                        newSize: selectedSize,
+                        quantity: product.quantity,
+                      })
+                    );
+                  }}
+                />
+              </div>
             </div>
-            <div className={styles.titleContainer}>
-              <p className={styles.brandName}>Boheamian Traders</p>
-              <a>{product.name}</a>
-              <p className={styles.size}>
-                Size: <span>{product.size}</span>
+          </div>
+          <div className={styles.innerSection}>
+            <div className={styles.secondSection}>
+              <p className={styles.title}>Price</p>
+              <p className={styles.price}>
+                $<span className={styles.us}>US</span> {product.price}
               </p>
-              <button
-                className={styles.change}
-                onClick={() => {
-                  setShowChange(true);
-                  dispatch(selectSize({ size: product.size }));
-                  setSelectedSizeForChange(product.size)
-                }}
-              >
-                CHANGE
-              </button>
-              <ChangeSizeModal
-                show={showChange}
-                onClose={() => setShowChange(false)}
-                allSize={product.allSize}
-                name={product.name}
-                onChangeSize={(size) => {
-                  dispatch(selectSize({ size: size }));
-                }}
-                onSave={() => {
-                  dispatch(
-                    changeSize({
-                      id: product.id,
-                      prevSize: selectedSizeForChange,
-                      newSize: selectedSize,
-                      quantity: product.quantity,
-                    })
-                  );
-                }}
-              />
             </div>
-          </div>
-          <div className={styles.secondSection}>
-            <p className={styles.title}>Price</p>
-            <p className={styles.price}>$US {product.price}</p>
-          </div>
-          <div className={styles.thirdSection}>
-            <p className={styles.title}>QuANTITY:</p>
-            <div className={styles.quantityContainer}>
-              <button
-                className={styles.quantityBtn}
-                onClick={() => {
-                  product.quantity !== 1
-                    ? dispatch(
-                        decrease({
+            <div className={styles.thirdSection}>
+              <p className={styles.title}>
+                Quantity<span className={styles.quantitySemiColon}>:</span>
+              </p>
+              <div className={styles.quantityContainer}>
+                <div className={styles.quantityInnerContainer}>
+                  <button
+                    className={styles.quantityBtn}
+                    onClick={() => {
+                      product.quantity !== 1
+                        ? dispatch(
+                            decrease({
+                              id: product.id,
+                              size: product.size,
+                              price: product.price,
+                            })
+                          )
+                        : dispatch(
+                            remove({
+                              id: product.id,
+                              size: product.size,
+                              price: product.price,
+                            })
+                          );
+                    }}
+                  >
+                    <img src={downArrow} alt="decrease" />
+                  </button>
+                  <p className={styles.quantitySmall}>{product.quantity}</p>
+                  <button
+                    className={styles.quantityBtn}
+                    onClick={() => {
+                      dispatch(
+                        addToCart({
                           id: product.id,
                           size: product.size,
                           price: product.price,
-                        })
-                      )
-                    : dispatch(
-                        remove({
-                          id: product.id,
-                          size: product.size,
-                          price: product.price,
+                          name: product.name,
+                          photo: product.photo,
                         })
                       );
-                }}
-              >
-                <img src={downArrow} alt="decrease" />
-              </button>
-              <p>{product.quantity}</p>
-              <button
-                className={styles.quantityBtn}
-                onClick={() => {
-                  dispatch(
-                    addToCart({
-                      id: product.id,
-                      size: product.size,
-                      price: product.price,
-                      name: product.name,
-                      photo: product.photo,
-                    })
-                  );
-                }}
-              >
-                <img src={upArrow} alt="increase" />
-              </button>
+                    }}
+                  >
+                    <img src={upArrow} alt="increase" />
+                  </button>
+                </div>
+                <p className={styles.quantityMedium}>{product.quantity}</p>
+              </div>
             </div>
-          </div>
-          <div className={styles.forthSection}>
-            <p className={styles.title}>Total</p>
-            <div>
-              <p className={styles.totalPrice}>
-                $US {product.price * product.quantity}
-              </p>
-              <img
-                className={styles.cross}
-                src={cross}
-                alt="delete"
-                onClick={() => {
-                  dispatch(
-                    remove({
-                      id: product.id,
-                      size: product.size,
-                      price: product.size,
-                    })
-                  );
-                }}
-              />
+            <div className={styles.forthSection}>
+              <p className={styles.title}>Total</p>
+              <div>
+                <p className={styles.totalPrice}>
+                  $<span className={styles.us}>US</span>{" "}
+                  {product.price * product.quantity}
+                </p>
+                <img
+                  className={styles.cross}
+                  src={cross}
+                  alt="delete"
+                  onClick={() => {
+                    dispatch(
+                      remove({
+                        id: product.id,
+                        size: product.size,
+                        price: product.size,
+                      })
+                    );
+                  }}
+                />
+              </div>
             </div>
           </div>
         </div>
@@ -191,7 +211,9 @@ const Cart = () => {
       <div className={styles.checkoutContainer}>
         <div className={styles.innerContainer}>
           <p className={styles.checkoutTitle}>SUBTOTAL:</p>
-          <p className={styles.price}>$US {totalPrice}</p>
+          <p className={styles.price}>
+            $<span className={styles.us}>US</span> {totalPrice}
+          </p>
         </div>
         <div className={styles.innerContainer}>
           <p className={styles.checkoutTitle}>SHIPPING:</p>
@@ -201,7 +223,9 @@ const Cart = () => {
         </div>
         <div className={styles.innerContainer}>
           <p className={styles.checkoutTitle}>TAX:</p>
-          <p className={styles.price}>$US {tax}</p>
+          <p className={styles.price}>
+            $<span className={styles.us}>US</span> {tax}
+          </p>
         </div>
         <div className={styles.innerContainer}>
           <p className={styles.checkoutTitle}>COUPON CODE:</p>
@@ -211,7 +235,9 @@ const Cart = () => {
         </div>
         <div className={styles.innerContainer}>
           <p className={styles.checkoutTitle}>GRAND TOTAL:</p>
-          <p className={styles.grandTotal}>$US {+totalPrice + +tax}</p>
+          <p className={styles.grandTotal}>
+            $<span className={styles.us}>US</span> {+totalPrice + +tax}
+          </p>
         </div>
         <div className={styles.zip}>
           <p>ZIP IT NOW, PAY LATER</p>
