a4de41504f8a311203ab803857d7613b2b9a52ad
Morteza erfani <m.erfani74@gmail.com>
2022-10-27 01:06:09 +0330

    add comments to contact us page and add call link and mail link to phone numbers and email addresses

--- src/components/contactUs/ContactUs.js
+++ src/components/contactUs/ContactUs.js
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+// Google recaptcha
 import ReCaptchaV2 from "react-google-recaptcha";
 
 // Styles
@@ -9,29 +12,35 @@ import redCross from "../../assets/redCross.svg";
 
 // functions
 import { validateEmail } from "../../helpers/functions";
-import { Link } from "react-router-dom";
 
 const ContactUs = () => {
+  // set email validation
   const [isValid, setIsValid] = useState(true);
+  // set user email
   const [email, setEmail] = useState("");
+  //set user comment
   const [comment, setComment] = useState("0");
 
+  // set page Title
   useEffect(() => {
     document.title = "Contact Us";
   });
 
+  // check if input value is not empty return valid style class name
   const isEmpty = (value) =>
     value !== "" && value !== "0" ? styles.green : "";
 
+  // send user comment data to server
   const clickHandler = (event) => {
     event.preventDefault();
     if (comment && email && isValid) {
-      // codes to send data to backend
+      // codes to send data to server
     }
   };
 
   return (
     <div className={styles.container}>
+      {/* user location in site */}
       <p className={styles.location}>
         <Link to="/">HOME</Link> / CONTACT US
       </p>
@@ -55,10 +64,10 @@ const ContactUs = () => {
           Erina Heights, NSW, 2260
         </div>
         <div>
-          Ph: <a>+61 2 4367 0709</a>
+          Ph: <a href="tel:+61 2 4367 0709">+61 2 4367 0709</a>
         </div>
         <div>
-          <a>erina.boutique@bohemiantraders.com</a>
+          <a href="mailto:erina.boutique@bohemiantraders.com">erina.boutique@bohemiantraders.com</a>
         </div>
         <div>
           Mon-Sat 8:30am - 5:00pm
@@ -73,9 +82,9 @@ const ContactUs = () => {
           Cooks Hill, NSW, 2300
         </div>
         <div>
-          Ph: <a>+61 2 8376 2179</a>
+          Ph: <a href="tel:+61 2 8376 2179">+61 2 8376 2179</a>
         </div>
-        <a>darby.boutique@bohemiantraders.com</a>
+        <a href="mailto:darby.boutique@bohemiantraders.com">darby.boutique@bohemiantraders.com</a>
         <div>
           Mon-Sat 9:00am - 5:00pm
           <br />
@@ -89,31 +98,33 @@ const ContactUs = () => {
         </div>
         <div>
           We're here to help! Contact us via the form below, or email us here:
-          <a>customerservice@bohemiantraders.com</a>, alternatively, feel free
-          to call us on <a>+61 2 4327 8640</a>
+          <a href="mailto:customerservice@bohemiantraders.com">customerservice@bohemiantraders.com</a>, alternatively, feel free
+          to call us on <a href="tel:+61 2 4327 8640">+61 2 4327 8640</a>
         </div>
         <div className={styles.question}>
           Do you have a question about the status of an existing online order?
         </div>
         <div>
-          Please email us here: <a>fulfilment@bohemiantraders.com</a>, or call
-          us on <a>+61 2 4327 8640</a>, Mon-Fri 9am - 5pm
+          Please email us here: <a href="mailto:fulfilment@bohemiantraders.com">fulfilment@bohemiantraders.com</a>, or call
+          us on <a href="tel:+61 2 4327 8640">+61 2 4327 8640</a>, Mon-Fri 9am - 5pm
         </div>
         <div className={styles.question}>
           For all PR and Marketing enquiries:
         </div>
         <div>
-          Please email us here: <a>pr@bohemiantraders.com</a>, or call us on{" "}
-          <a>+61 2 4327 8640</a>
+          Please email us here: <a href="mailto:pr@bohemiantraders.com">pr@bohemiantraders.com</a>, or call us on{" "}
+          <a href="tel:+61 2 4327 8640">+61 2 4327 8640</a>
         </div>
         <div className={styles.question}>
           Enquiring about Wholesale of Bohemian Traders:
         </div>
         <div>
-          Please email us here: <a>wholesale@bohemiantraders.com</a>, or call us
-          on <a>+61 2 4327 8640</a>
+          Please email us here: <a href="mailto:wholesale@bohemiantraders.com">wholesale@bohemiantraders.com</a>, or call us
+          on <a href="tel:+61 2 4327 8640">+61 2 4327 8640</a>
         </div>
       </div>
+
+      {/* comment form */}
       <form className={styles.form}>
         <div className={styles.formInnerContainer}>
           <div>
@@ -145,12 +156,14 @@ const ContactUs = () => {
               inputMode="email"
               name="email"
               onChange={(e) => {
+                // set email
                 setEmail(e.target.value);
+                // validate email and set the result
                 setIsValid(validateEmail(email));
-                // console.log(email);
               }}
+              // validate email and set the result
               onBlur={() => setIsValid(validateEmail(email))}
-              className={isValid ? isEmpty(email) : styles.invalid}
+              className={isValid ? isEmpty(email) : styles.invalid} // validate email and change the input style
             />
             <div className={styles.err}>
               <p className={isValid ? "" : styles.show}>
@@ -170,9 +183,11 @@ const ContactUs = () => {
           rows="5"
           cols="50"
           onChange={(e) => {
+            // set comment
             setComment(e.target.value);
           }}
-          className={comment ? isEmpty(comment) : styles.invalid}
+          className={comment ? isEmpty(comment) : styles.invalid} // validate comment and change the input style
+          //set comment
           onBlur={(e) => setComment(e.target.value)}
         />
         <div className={styles.err}>
@@ -187,6 +202,7 @@ const ContactUs = () => {
         <button
           type="submit"
           className={styles.submit}
+          // send user comment data to server
           onClick={(e) => clickHandler(e)}
         >
           SUBMIT FORM
