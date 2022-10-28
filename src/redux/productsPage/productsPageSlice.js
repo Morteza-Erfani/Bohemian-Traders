7ff0b8b999f5247c942b2bc9b20cb45aba50547f
Morteza erfani <m.erfani74@gmail.com>
2022-10-28 22:43:13 +0330

    add comments to productPageSlice.js

--- src/redux/productsPage/productsPageSlice.js
+++ src/redux/productsPage/productsPageSlice.js
@@ -5,7 +5,7 @@ const initialState = {
   collection: "",
   test: undefined,
   title: "",
-  products: [],
+  products: [], // Storing all products
   filteredProducts: [],
   filters: {
     sizes: [],
@@ -44,9 +44,9 @@ export const productsPageSlice = createSlice({
       state.filters.sizes = state.filters.sizes.concat(action.payload.size);
     },
     removeSize: (state, action) => {
-      const sizes = state.filters.sizes;
-      const i = sizes.indexOf(action.payload.size);
-      sizes.splice(i, 1);
+      const sizes = state.filters.sizes; // Store all selected size for filter
+      const i = sizes.indexOf(action.payload.size); // Index of size to remove
+      sizes.splice(i, 1); // Remove size
       state.filters.sizes = sizes;
     },
     resetSize: (state) => {
@@ -68,54 +68,74 @@ export const productsPageSlice = createSlice({
       state.view = action.payload.view;
     },
     runSizeFilter: (state) => {
-      const sizeFiltered = [];
+      const sizeFiltered = []; // temporary variable for storing filtered product by size
+      // Check to no filter run before
       if (state.filteredProducts.length === 0) {
+        // Check for at least one size selected
         if (state.filters.sizes.length !== 0) {
-          let filterIndex = new Set();
+          let filterIndex = new Set(); // variable for all index of products that have a special size
+          // Map through all selected size
           state.filters.sizes.map((filterSize) => {
+            // Loop through all products
             for (let [index, product] of state.products.entries()) {
+              // Loop through all size of a product
               product.sizes.map((size) => {
                 if (filterSize.toUpperCase() === size.name) {
-                  filterIndex.add(index);
+                  filterIndex.add(index); // Add index of product
                 }
               });
             }
           });
+          // Loop through all product indexes that has selected sizes
           filterIndex.forEach((index) => {
             state.filteredProducts.push(state.products[index]);
             filteredProductsBeforeSort = state.filteredProducts;
           });
-        } else {
+        }
+        // If no size selected return no filter product to use all products in page
+        else {
           state.filteredProducts = [];
           filteredProductsBeforeSort = state.products;
         }
-      } else {
+      }
+      // If any filter run before
+      else {
+        // Check for at least one size selected
         if (state.filters.sizes.length !== 0) {
-          let filterIndex = new Set();
+          let filterIndex = new Set(); // variable for all index of products that have a special size
+          // Map through all selected size
           state.filters.sizes.map((filterSize) => {
+            // Loop through all products
             for (let [index, product] of state.filteredProducts.entries()) {
+              // Loop through all size of a product
               product.sizes.map((size) => {
                 if (filterSize.toUpperCase() === size.name) {
-                  filterIndex.add(index);
+                  filterIndex.add(index); // Add index of product
                 }
               });
             }
           });
+          // Loop through all product indexes that has selected sizes
           filterIndex.forEach((index) => {
             sizeFiltered.push(state.filteredProducts[index]);
           });
           state.filteredProducts = sizeFiltered;
           filteredProductsBeforeSort = state.filteredProducts;
-        } else {
+        }
+        // If no size selected return no filter product to use all products in page
+        else {
           state.filteredProducts = [];
           filteredProductsBeforeSort = state.products;
         }
       }
     },
     runPriceFilter: (state) => {
-      const priceFiltered = [];
+      const priceFiltered = []; // temporary variable for storing filtered product by price
+      // Check to no filter run before
       if (state.filteredProducts.length === 0) {
+        // Loop through all products
         state.products.map((product) => {
+          // Check if price is in selected range
           if (
             product.price > state.filters.price.min &&
             product.price < state.filters.price.max
@@ -124,8 +144,12 @@ export const productsPageSlice = createSlice({
           }
           filteredProductsBeforeSort = state.filteredProducts;
         });
-      } else {
+      }
+      // If any filter run before
+      else {
+        // Loop through all filtered products
         state.filteredProducts.map((product) => {
+          // Check if price is in selected range
           if (
             product.price > state.filters.price.min &&
             product.price < state.filters.price.max
@@ -138,24 +162,33 @@ export const productsPageSlice = createSlice({
       }
     },
     sort: (state) => {
+      // If no sort option being selected
       if (state.sortOption === "") {
         state.products = productsBeforeSort;
         state.filteredProducts = filteredProductsBeforeSort;
-      } else {
+      }
+      // If a sort option selected
+      else {
         if (state.sortOption === "PRICE: ASCENDING") {
+          // Check to no filter run before
           if (state.filteredProducts.length === 0) {
             state.filteredProducts = state.products.sort(
               (a, b) => a.price - b.price
             );
-          } else {
+          }
+          // If any filter run before
+          else {
             state.filteredProducts.sort((a, b) => a.price - b.price);
           }
         } else if (state.sortOption === "PRICE: DESCENDING") {
+          // Check to no filter run before
           if (state.filteredProducts.length === 0) {
             state.filteredProducts = state.products.sort(
               (a, b) => b.price - a.price
             );
-          } else {
+          }
+          // If any filter run before
+          else {
             state.filteredProducts.sort((a, b) => b.price - a.price);
           }
         }
